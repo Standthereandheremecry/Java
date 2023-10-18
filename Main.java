844adb6d462ae8028752ccfdb02fa2b29a530c0f
Standthereandheremecry <yanganganglin@gmail.com>
2023-10-18 20:16:27 +0800

    test

--- Main.java
+++ Main.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.scanner;
 
 public class Main {
     public static void main(String[] args) {
